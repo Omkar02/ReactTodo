5e8f67dae5c97466760d5bb686b4faded3bf7e2f
Omkar02 <omkjoshi.dev.07@gmail.com>
2024-03-14 21:07:11 +0530

    init the project

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+    singleQuote: true,
+    trailingComma: 'all',
+    tailwindConfig: './tailwind.config.js',
+    tabWidth: 4,
+};
+
+module.exports = config;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}', // Adjust file paths to match your project structure
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
