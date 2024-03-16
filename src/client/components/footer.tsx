878c7c4416fbfd93ed442bfacdc008d05c9e1fa7
Omkar02 <omkjoshi.dev.07@gmail.com>
2024-03-16 21:21:12 +0530

    created footer component with the project github link

---
+++ src/client/components/footer.tsx
@@ -0,0 +1,44 @@
+interface Technology {
+    name: string;
+    logoSrc: string; // Placeholder for logo image URL
+}
+
+const technologies: Technology[] = [
+    { name: 'React', logoSrc: 'React-icon.svg' },
+    { name: 'TypeScript', logoSrc: 'ts-logo-128.svg' },
+    { name: 'Vite', logoSrc: 'Vitejs-logo.svg' },
+    { name: 'Tailwind CSS', logoSrc: 'tailwindcss-mark.svg' },
+];
+
+export default function Footer() {
+    return (
+        <footer
+            className="m-0 w-full bg-zinc-800 
+            text-white text-center p-4 flex items-center justify-between"
+        >
+            <div className="text-left text-blue-200">
+                <p>Made by Omkar Joshi</p>
+                <p>
+                    GitHub Link:{' '}
+                    <a
+                        href="https://github.com/Omkar02/ReactTodo"
+                        className="underline text-blue-400"
+                    >
+                        ReactTodo
+                    </a>
+                </p>
+            </div>
+
+            <div className="flex items-center">
+                {technologies.map((tech) => (
+                    <img
+                        key={tech.name}
+                        src={tech.logoSrc}
+                        alt={tech.name}
+                        className="w-8 h-8 mr-2"
+                    />
+                ))}
+            </div>
+        </footer>
+    );
+}
